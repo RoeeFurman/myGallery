17f9b074974efd0f5f60cb8f4f1ab397ec2593d9
Roee Furman <roeefurman1@gmail.com>
2022-02-07 19:37:09 +0200

    new pics

--- js/gallery-service.js
+++ js/gallery-service.js
@@ -7,9 +7,12 @@ var gProjs;
 function createProjects(projectsNum) {
     var projects = [];
     
-    projects[0] = createProject('pacman', 'Pacman','best game ever', 'very good practice', 'https://roeefurman.github.io/Pacman/', 'pacman.jfif');
+    projects[0] = createProject('pacman', 'Pacman','best game ever', 'very good practice', 'https://roeefurman.github.io/Pacman/', 'pacman1.png');
     projects[1] = createProject('MineSweeper', 'MineSweeper','best game ever', 'very good practice', 'https://roeefurman.github.io/Mine-Sweeper/',
-     'minesweeper.jpeg');
+    'minesweeper.jpeg');
+    projects[2] = createProject('Touch-Nums', 'Touch-Nums','best game ever', 'very good practice', '','touch-nums.png');
+    projects[3] = createProject('Ball-board', 'Ball-board','best game ever', 'very good practice', '','ballboard.png');
+    projects[4] = createProject('Chess', 'Chess','best game ever', 'very good practice', '','chess.png');
     // for (var i = 0; i < projectsNum; i++) {
     //     projects[i] = createProject('pacman', 'pacman',
     //         'best game ever', 'very good practice',
